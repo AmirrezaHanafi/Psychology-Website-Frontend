efb398071aef898ccc808c130fb033763e215fdf
AmirrezaHanafi <amirreza1688@gmail.com>
2026-04-27 12:17:24 +0330

    [ADD]routing to homepage and services and about us and 404 notgound page

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,15 @@
+import Header from '../components/Header.jsx'
+import { Outlet } from 'react-router-dom'
+import Footer from '../components/Footer.jsx'
+
+export default function Layout() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  )
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      home page
+      <span>asdas</span>
+    </div>
+  )
+}
