5bc6943b4298dd86fd344dbc7e44336671a69e7a
AmirrezaHanafi <amirreza1688@gmail.com>
2026-04-30 17:08:51 +0330

    [FIX] logo test

--- src/components/Header/DesktopHeader.jsx
+++ src/components/Header/DesktopHeader.jsx
@@ -1,12 +1,11 @@
 import { Link } from 'react-router-dom'
 import Button from '../Buttons/Button.jsx'
-import Image from '/public/assets/Logo/HeaderLogo.png'
 
 export default function DesktopHeader() {
   return (
     <header className="bg-primary sticky top-20 right-0 left-0 z-50 mx-auto mt-7 hidden h-24 w-[98%] items-center justify-between rounded-3xl px-5 py-0 backdrop-blur-[6px] md:flex lg:w-[96%] lg:px-10 lg:py-5">
       <Link to={'/'} className="mt-5 flex items-center justify-center">
-        <img src={Image} alt="Logo" />
+        <img src="/assets/HeaderLogo.png" alt="Logo" />
         <div className="flex flex-col items-center justify-center">
           <span className="font-Prata block text-2xl">OPAL MIND</span>
           <span className="font-Prata text-center text-base">PSYCHIATRIST</span>
